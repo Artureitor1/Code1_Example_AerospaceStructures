7bcfdfe2c561e4406b8cc8d960821268f1142cf2
Artureitor1 <109309322+Artureitor1@users.noreply.github.com>
2022-07-31 19:45:36 +0200

    Refactoring_V2
    Bugs has been fixed and the class has been tested. Also plot functions has been implemented in the class

--- code_A/SolverStructure.m
+++ code_A/SolverStructure.m
@@ -36,42 +36,39 @@ classdef SolverStructure < handle
     end
 
     methods (Access = public)
-        function solver(cParams)
-            obj.init(obj,cParams);
+        function solver(obj,cParams)
+            obj.init(cParams);
         end
-
         function Grahps(obj)
-            obj.Represent(obj)
+            obj.Represent()
         end  
     end
 
     methods (Access = protected)
 
-        function init(obj,cParams);
-            obj.InputData(obj,cParams);
-            obj.InputStructure(obj,cParams);
-            obj.SolveStructure(obj);
+        function init(obj,cParams)
+            obj.InputData(cParams);
+            obj.InputStructure(cParams);
+            obj.SolveStructure();
         end
-
+        
         function SolveStructure(obj)
             obj.Dimensions()
-            Td = connectDOFs(obj.n_el,obj.n_nod,obj.n_i,obj.Tn);
-            Kel = computeKelBar(obj.n_d,obj.n_el,obj.n_el_dof,obj.x,obj.Tn,obj.mat,obj.Tmat);
-            obj.KG = assemblyKG(obj.n_el,obj.n_el_dof,obj.n_dof,Td,Kel); 
-            obj.Fext = computeF(obj.n_i,obj.n_dof,obj.Fdata);
-            [vL,vR,uR] = applyCond(obj.n_i,obj.n_dof,obj.fixNod);
+            Td = obj.connectDOFs(obj.n_el,obj.n_nod,obj.n_i,obj.Tn);
+            Kel = obj.computeKelBar(obj.n_d,obj.n_el,obj.n_el_dof,obj.x,obj.Tn,obj.mat,obj.Tmat);
+            obj.KG = obj.assemblyKG(obj.n_el,obj.n_el_dof,obj.n_dof,Td,Kel); 
+            obj.Fext = obj.computeF(obj.n_i,obj.n_dof,obj.Fdata);
+            [vL,vR,uR] = obj.applyCond(obj.n_i,obj.n_dof,obj.fixNod);
             [obj.u,obj.R] = obj.SolveSystem(vL,vR,uR,obj.KG,obj.Fext); %This function is defined in the subclasses!!!!
-            [obj.eps,obj.sig] = computeStrainStressBar(obj.n_d,obj.n_el,obj.u,Td,obj.x,obj.Tn,obj.mat,obj.Tmat);
-            [obj.FB] = bucklingFailure(obj.mat,obj.Tmat,obj.x,obj.Tn,obj.n_el, obj.sig);
+            [obj.eps,obj.sig] = obj.computeStrainStressBar(obj.n_d,obj.n_el,obj.u,Td,obj.x,obj.Tn,obj.mat,obj.Tmat);
+            [obj.FB] = obj.bucklingFailure(obj.mat,obj.Tmat,obj.x,obj.Tn,obj.n_el, obj.sig);
         end
-
         function Represent(obj)
-            plotDisp(obj.n_d,obj.n,obj.u,obj.x,obj.Tn,15);
-            plotStrainStress(obj.n_d,obj.eps,obj.x,obj.Tn,{'Strain'});
-            plotStrainStress(obj.n_d,obj.sig,obj.x,obj.Tn,{'Stress';'(Pa)'});
-            plotBarStressDef(obj.x,obj.Tn,obj.u,obj.sig,15)
+            obj.plotDisp(obj.n_d,obj.n,obj.u,obj.x,obj.Tn,15);
+            obj.plotStrainStress(obj.n_d,obj.eps,obj.x,obj.Tn,{'Strain'});
+            obj.plotStrainStress(obj.n_d,obj.sig,obj.x,obj.Tn,{'Stress';'(Pa)'});
+            obj.plotBarStressDef(obj.x,obj.Tn,obj.u,obj.sig,15)
         end
-
         function InputData(obj,cParams)
             obj.F               = cParams.F;
             obj.Young           = cParams.Young;
@@ -79,7 +76,6 @@ classdef SolverStructure < handle
             obj.thermal_coeff   = cParams.thermal_coeff;
             obj.Inertia         = cParams.Inertia;
         end
-
         function InputStructure(obj,cParams)
             obj.Fdata(:,3)  = cParams.Fdata(:,3) * obj.F;
             obj.x       = cParams.x;
@@ -100,8 +96,8 @@ classdef SolverStructure < handle
         end
     end
 
-    methods (Access = public, Static)
-                function Td = connectDOFs(n_el,n_nod,n_i,Tn)
+    methods (Access = protected, Static)
+        function Td = connectDOFs(n_el,n_nod,n_i,Tn)
                         Td=zeros(n_el,n_nod*n_i);
                         for i=1:n_el
                            for j=1:n_nod*n_i
@@ -116,7 +112,7 @@ classdef SolverStructure < handle
                         end                        
                 end
                 
-                function Kel = computeKelBar(n_d,n_el,n_el_dof,x,Tn,mat,Tmat)
+        function Kel = computeKelBar(n_d,n_el,n_el_dof,x,Tn,mat,Tmat)
         
                         Kel=zeros(n_el_dof,n_el_dof,n_el);
                             for e=1:n_el
@@ -141,7 +137,8 @@ classdef SolverStructure < handle
                                 
                             end
                 end
-                function KG = assemblyKG(n_el,n_el_dof,n_dof,Td,Kel)
+        
+        function KG = assemblyKG(n_el,n_el_dof,n_dof,Td,Kel)
                     KG=zeros(n_dof,n_dof);
                             for e=1:n_el
                                 for i=1:2*2 
@@ -153,7 +150,8 @@ classdef SolverStructure < handle
                                 end
                             end
                 end
-                function Fext = computeF(n_i,n_dof,Fdata)
+                
+        function Fext = computeF(n_i,n_dof,Fdata)
                     Fext=zeros(n_dof,1); 
                             for i=1:length(Fdata)
                                 if Fdata(i,2) == 2 % even case
@@ -164,7 +162,7 @@ classdef SolverStructure < handle
                             end
                 end
         
-                function [vL,vR,uR] = applyCond(n_i,n_dof,fixNod)
+        function [vL,vR,uR] = applyCond(n_i,n_dof,fixNod)
                     uR = zeros(size(fixNod,1),1);
                     vR = zeros(size(fixNod,1),1);
                     v = linspace(1,16,16);
@@ -180,11 +178,11 @@ classdef SolverStructure < handle
                     vL = transpose (setdiff(v,vR));
                 end
         
-                function [u,R] = SolveSystem(vL,vR,uR,KG,Fext)
+        function [u,R] = SolveSystem(vL,vR,uR,KG,Fext)
                    error('SolveSystem function not implemented. This class is not for use. Try SolverStructureDirect class or SolverStructureIterative class')
                 end
         
-                function [eps,sig] = computeStrainStressBar(n_d,n_el,u,Td,x,Tn,mat,Tmat)
+        function [eps,sig] = computeStrainStressBar(n_d,n_el,u,Td,x,Tn,mat,Tmat)
                             eps=zeros(n_el,1);
                             sig=zeros(n_el,1);
                             
@@ -210,7 +208,8 @@ classdef SolverStructure < handle
                                 sig(e,1)=mat(Tmat(e),1)*eps(e,1);
                             end
                 end
-                function [FB] = bucklingFailure(mat,Tmat,x,Tn,n_el, sig)
+        
+        function [FB] = bucklingFailure(mat,Tmat,x,Tn,n_el, sig)
                    FB = zeros(n_el,1); 
                         for e = 1:n_el
                     
@@ -233,9 +232,88 @@ classdef SolverStructure < handle
                             end
                         end
                 end
+    
+        function plotStrainStress(n_d,s,x,Tn,title_name)
 
+                for i = 1:n_d
+                    X0{i} = reshape(x(Tn,i),size(Tn))';
+                end
+                S = repmat(s',size(Tn,2),1);
+                
+                figure('color','w');
+                hold on;       
+                box on;        
+                axis equal;    
+                colormap jet;  
+                
+                xlabel('x (m)')
+                ylabel('y (m)')
+                title(title_name{1});
+                
+                patch(X0{:},S,'edgecolor','flat','linewidth',2);
+                
+                caxis([min(S(:)),max(S(:))]);
+                cbar = colorbar;
+                set(cbar,'Ticks',linspace(min(S(:)),max(S(:)),5));
+                title(cbar,title_name);
         end
-    end
-end
+    
+        function plotDisp(n_d,n,u,x,Tn,fact)
 
+                U = reshape(u,n_d,n);
+                for i = 1:n_d
+                    X0{i} = reshape(x(Tn,i),size(Tn))';
+                    X{i} = X0{i}+fact*reshape(U(i,Tn),size(Tn))';
+                end
+                D = reshape(sqrt(sum(U(:,Tn).^2,1)),size(Tn))';
+                
+                figure('color','w');
+                hold on;       
+                box on;        
+                axis equal;    
+                colormap jet;  
+                
+                xlabel('x (m)')
+                ylabel('y (m)')
+                title('Displacement');
+                
+                patch(X0{:},zeros(size(D)),'edgecolor',[0.5,0.5,0.5],'linewidth',2);
+                
+                patch(X{:},D,'edgecolor','interp','linewidth',2);
+                
+                caxis([min(D(:)),max(D(:))]);
+                cbar = colorbar;              
+                set(cbar,'Ticks',linspace(min(D(:)),max(D(:)),5))
+                end
+    
+        function plotBarStressDef(x,Tn,u,sig,scale)
+                n_d = size(x,2);
+                X = x(:,1);
+                Y = x(:,2);
+                ux = u(1:n_d:end);
+                uy = u(2:n_d:end);
+                
+                figure('color','w');
+                hold on
+                box on
+                axis equal;
+                colormap jet;
+                
+                plot(X(Tn)',Y(Tn)','-k','linewidth',0.5);
+                
+                patch(X(Tn)'+scale*ux(Tn)',Y(Tn)'+scale*uy(Tn)',[sig';sig'],'edgecolor','flat','linewidth',2);
+                
+                xlabel('x (m)')
+                ylabel('y (m)')
+                
+                title(sprintf('Deformed structure (scale = %g)',scale));
+                
+                caxis([min(sig(:)),max(sig(:))]);
+                cbar = colorbar;
+                set(cbar,'Ticks',linspace(min(sig(:)),max(sig(:)),5));
+                title(cbar,{'Stress';'(Pa)'});
+                
+        end
+    end   
+end
 
